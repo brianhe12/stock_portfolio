212b768366f27cb02b212f570b15ea23ee5325e9
brianhe12 <brian.he17@gmail.com>
2020-03-15 12:36:35 -0400

    made change in database structure

--- my-app/src/server/mongo_function.py
+++ my-app/src/server/mongo_function.py
@@ -15,6 +15,8 @@ db.user.insert({
 
 # This query returns JSON if exists, and None if it doesn't exist
 exists = db.users.find_one({ "$and": [ { "email": "test@test.com" }, { "portfolio.NFLX":{"$exists":True} } ] })
+
+https://stackoverflow.com/questions/34179117/how-can-i-increment-an-array-element-referenced-by-index-in-mongodb
 """
 
 import datetime
@@ -24,7 +26,7 @@ import os
 from pymongo import MongoClient
 from pprint import pprint
 
-def buy_sell_stock(user,stock,quantity,operation):
+def buy_sell_stock(user,stock,amount,operation):
 
     load_dotenv()
     # Connect with Mongodb Atlas
@@ -37,31 +39,40 @@ def buy_sell_stock(user,stock,quantity,operation):
         print("User does not exist, Adding User")
         db.users.insert_one({
             "email": user,
-            "portfolio": {},
+            "portfolio": [],
             "date": str(datetime.datetime.utcnow()),
             "cash": 5000
         })
     
     if operation == "Sell":
-        quantity = quantity * -1
+        amount = amount * -1
 
     # Checks to see if a user with a certain email has an entry for the stock they are trying to Buy/Sell
-    exists = db.users.find_one({ "$and": [ { "email": user }, { "portfolio." + stock:{"$exists":True} } ] })
+    exists = db.users.find_one({ "$and": [ {"portfolio.symbol": stock}, {"email": user} ] })
 
-    # If query exists, we can check index
-    if exists != None:
-        if result['portfolio'][stock] + quantity < 0:
-            print("You do not have enough " + stock + " stock for this action")
-            return result
+    # If query exists, and we try to sell
+    if exists != None and operation == "Sell":
+        for i in range(len(result["portfolio"])):
+            if result['portfolio'][i]['amount'] + amount < 0 and result['portfolio'][i]['symbol'] == stock:
+                print("You do not have enough " + stock + " stock for this action")
+                return result
     # If query does not exist & we try to sell, throw an error
     elif exists == None and operation == "Sell":
         print("No inventory of " + stock + " to sell.")
         return result
 
-    # Update and Increase/Decrease user holdings of $stock by $amount
-    query = { "email": user }
-    newValues = { "$inc": { "portfolio." + stock: quantity } }
-    db.users.update_one(query, newValues)
+    # If query does not exist & we try to buy, add it in
+    elif exists == None and operation == "Buy":
+        db.users.update_one({"email": user}, {"$push":{"portfolio": { "symbol": stock,"amount" : amount,"currentPrice": 3000 }}})
+        return db.users.find_one({"email": user})
+
+    # Update if exists
+    index = 0
+    for i in range(len(result["portfolio"])):
+        if result['portfolio'][i]['symbol'] == stock:
+            index = i
+            break
+    db.users.update_one({"email": user}, {"$inc":{"portfolio." + str(index) +".amount": amount}})
     return db.users.find_one({"email": user})
     
 
@@ -69,7 +80,22 @@ def buy_sell_stock(user,stock,quantity,operation):
 # client = MongoClient(os.getenv("MONGO_STRING"))
 # db=client.userHoldings
 
-# result = buy_sell_stock("test2@test.com","AMZN",16,"Buy")
+# result = buy_sell_stock("test3@test.com","APPL",20,"Sell")
 # print(result)
 
+
+# db.users.insert_one({
+#             "email": "NewTest@test.com",
+#             "portfolio": [
+#             {"symbol": "GOOG","amount": 4,"currentPrice": 3500},
+#             {"symbol": "AMZN","amount": 5,"currentPrice": 32500},
+#             ],
+#             "date": str(datetime.datetime.utcnow()),
+#             "cash": 5000
+#         })
+
+#result = db.users.find_one({"email": "NewTest@test.com"})
+#db.users.update_one({"email": "NewTest@test.com"},{"$inc": {"items." + str(2) + ".count": 1}}) 
+#db.users.update_one({"email": "NewTest@test.com"}, {"$inc":{"portfolio.0.amount": 2}})
+#print(db.users.find_one({"email": "NewTest@test.com"}))
 # print('Finish')
