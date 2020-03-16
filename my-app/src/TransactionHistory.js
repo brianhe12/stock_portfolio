b7f09a3a6eb1cafe42af847f273ac1be3f85e790
brianhe12 <brian.he17@gmail.com>
2020-03-16 19:35:38 -0400

    fixed firebase bug!

--- my-app/src/TransactionHistory.js
+++ my-app/src/TransactionHistory.js
@@ -4,14 +4,29 @@ import Cards from './components/cards'
 import app from "./base";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            transactionHistory: [],
+        }
+    }
     componentDidMount() {
         app.auth().onAuthStateChanged(user => {
             if (user) {
                 //User is signed in
-                console.log('This is the user: ', user)
+                console.log('This is the user: ', user.email)
                 this.setState({
                     isLoggedIn: true,
                 });
+                var url = 'http://127.0.0.1:5000/transactionHistory/' + user.email;
+                fetch(url)
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(data)
+                    this.setState({ transactionHistory: data })
+                })
+                .catch(console.log)
             } else {
                 // No user is signed in.
                 console.log('There is no logged in user');
@@ -20,34 +35,8 @@ class App extends Component {
                 });
             }
         });
-      fetch('http://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ contacts: data })
-      })
-      .catch(console.log)
     }
-  
-    state = {
-        isLoggedIn: false,
-        contacts: [],
-        transactionHistory: [
-            {
-              "stock": "APPL",
-              "transaction": "Buy",
-              "numShares": 3,
-              "pricePerShare": 338,
-              "Time": "10:23:01"
-            },
-            {
-              "stock": "GOOG",
-              "transaction": "Buy",
-              "numShares": 32,
-              "pricePerShare": 338,
-              "Time": "10:23:01"
-            },
-        ]
-    }
+
     render() {
         var isLoggedIn = this.state.isLoggedIn;
         return (
