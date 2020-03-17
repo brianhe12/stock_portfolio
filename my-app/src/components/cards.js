df5f540320f87ca66316ab13a81a9b3afe2481ce
brianhe12 <brian.he17@gmail.com>
2020-03-16 21:39:07 -0400

    touched up presentation of transaction history

--- my-app/src/components/cards.js
+++ my-app/src/components/cards.js
@@ -7,10 +7,10 @@ const Cards = ({ transactionHistory }) => {
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">{data.stock}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{data.transaction}</h6>
-              <p class="card-text">{data.numShares}</p>
+              <h6 class="card-subtitle mb-2 text-muted">Market {data.transaction}</h6>
+              <p class="card-text">{data.numShares} Shares</p>
               <p class="card-text">${data.pricePerShare}</p>
-              <p class="card-text">{data.Time}</p>
+              <p class="card-text">Filled {data.Time}, EST</p>
             </div>
           </div>
         ))}
